72b56bcf067d2f907a0592d35168d2ac6b47ed5b
Jenny Danzmayr <mail@evilscientress.de>
2024-12-18 23:27:52 +0100

    fixed import order in asgi.py

--- src/c3ds/asgi.py
+++ src/c3ds/asgi.py
@@ -15,11 +15,12 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from c3ds.urls import websocket_urlpatterns
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'c3ds.settings.production')
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
+
+from c3ds.urls import websocket_urlpatterns
+
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
