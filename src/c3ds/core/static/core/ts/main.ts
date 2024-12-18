852894d2611db4e1ae49edcedc03ee6e881eed9c
Jenny Danzmayr <mail@evilscientress.de>
2024-12-18 22:54:35 +0100

    fix handling of websocket url for old browser

--- src/c3ds/core/static/core/ts/main.ts
+++ src/c3ds/core/static/core/ts/main.ts
@@ -41,10 +41,10 @@ class WebScoketClient {
 
   connect() {
     this.ws = new WebSocket(
-      '//'
+      window.location.protocol === 'https' ? 'wss://' : 'ws://'
       + window.location.host
       + '/ws/display/'
-      + displaySlug
+      + this.displaySlug
       + '/'
     )
     this.ws.onmessage = (e) => {
